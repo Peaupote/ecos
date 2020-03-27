491a61a7e7a43da819014061f53004cea5603ecc
Benjamin Bonneau <bbonneau@clipper.ens.fr>
2020-03-27 10:10:00 +0100

    Generate fs iso without su, fix kernel size limit
    Requiert `e2tools`

--- tests/ext2/build-home.sh
+++ tests/ext2/build-home.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-MNT=/mnt
+# requiert `e2tools`
+
 IMG=home.img
 BASE=home
 
@@ -9,18 +10,22 @@ then
     echo "Partition $IMG already exists";
     dumpe2fs $IMG
     exit 0;
-else
-    echo "Creating partition $IMG";
-    dd if=/dev/zero of=$IMG bs=64K count=1;
-    mke2fs $IMG
+fi
+   
+echo "Creating partition $IMG";
+dd if=/dev/zero of=$IMG bs=64K count=1;
+mke2fs $IMG
+
+if ! cd $BASE
+then
+	rm -f $IMG
 fi
 
-if [ ! -e $MNT ];
-then mkdir $MNT
-fi
+e2mkdir ../$IMG:mpouzet \
+	&& find ./ -type f -exec e2cp {} ../$IMG:{} \; \
+    && echo "Partition created !"
 
-mount $IMG $MNT &&
-    mkdir $MNT/mpouzet &&
-    cp -r $BASE/* $MNT/
-    umount $MNT &&
-    echo "Partition created !"
+if [ $? -ne 0 ]
+then
+	rm -f ../$IMG
+fi
