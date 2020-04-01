ec23e26d8ee7c3806acd6250c731432ae7a5d87b
Benjamin Bonneau <bbonneau@clipper.ens.fr>
2020-04-01 09:49:33 +0200

    Fix test ext2

--- tests/host/ext2/ext2_wrap.h
+++ tests/host/ext2/ext2_wrap.h
@@ -3,14 +3,17 @@
 
 #include<stdint.h>
 #include<stddef.h>
+#include<stdbool.h>
 
 int init_ext2(void* fs);
 
-void print_type(uint8_t type);
+bool is_curr_dir();
+void print_type(uint16_t type);
 int ls();
 void print_stat();
+void do_touch(char* s);
 void do_cd(const char* s);
-void do_mkdir(char* s);
+bool do_mkdir(char* s);
 void* save_area(size_t* sz);
 void dump();
 
