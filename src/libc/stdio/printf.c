6227bb5ee78def7d0edd8a01069c7477366aef39
Maxime Flin <mflin@clipper.ens.fr>
2020-05-05 20:51:08 +0200

    Check printf format

--- src/libc/stdio/printf.c
+++ src/libc/stdio/printf.c
@@ -7,6 +7,7 @@
 #include <libc/unistd.h>
 #include <libc/ctype.h>
 #include <util/misc.h>
+#include <libc/errno.h>
 
 static const char * const decimal_digits = "0123456789";
 static const char * const hex_digits = "0123456789ABCDEF";
@@ -121,84 +122,94 @@ int fpprintf(stringl_writer w, void* wi, const char* fmt, va_list ps) {
         uint8_t mod   = 0;
         while(true) {
             switch(* ++fmt) {
+            case 'l':
+                switch(* ++fmt) {
                 case 'l':
-                    switch(* ++fmt) {
-                        case 'l':
-                            mod = 2;
-                            continue;
-                        default:
-                            mod = 1;
-                            --fmt;
-                            continue;
-                    }
-                case '-':
-                    align_left = true;
-                    continue;
-                case '0':
-                    compl_char = '0';
+                    mod = 2;
                     continue;
                 default:
-                    if (isdigit(*fmt)) {
-                        if (!compl_char) compl_char = ' ';
-                        compl_len = *fmt - '0';
-                        while (isdigit(*++fmt))
-                            compl_len = compl_len * 10 + *fmt - '0';
-                        --fmt;
-                        continue;
-                    }
-                    break;
+                    mod = 1;
+                    --fmt;
+                    continue;
+                }
+            case '-':
+                align_left = true;
+                continue;
+            case '0':
+                compl_char = '0';
+                continue;
+
+            case 0:
+#ifndef __is_kernel
+                errno = EINVAL;
+#endif
+                return -1;
+
+            default:
+                if (isdigit(*fmt)) {
+                    if (!compl_char) compl_char = ' ';
+                    compl_len = *fmt - '0';
+                    while (isdigit(*++fmt))
+                        compl_len = compl_len * 10 + *fmt - '0';
+                    --fmt;
+                    continue;
+                }
+                break;
             }
             break;
         }
 
         size_t len;
         switch(*fmt) {
-            case 'c':
-                buf[0] = (char)va_arg(ps, int);
-                buf[1] = 0;
-                len    = 1;
-                if (compl_char && !align_left)
-                    count += print_complete(buf, w, wi,
-                                compl_len - 1, compl_char, 256);
-                (*w)(wi, buf, 1);
-                count += 1;
+        case 'c':
+            buf[0] = (char)va_arg(ps, int);
+            buf[1] = 0;
+            len    = 1;
+            if (compl_char && !align_left)
+                count += print_complete(buf, w, wi,
+                                        compl_len - 1, compl_char, 256);
+            (*w)(wi, buf, 1);
+            count += 1;
             goto print_comp;
-            case 's':{
-                const char *s = va_arg(ps, const char*);
-                len = strlen(s);
-                if (compl_char && !align_left)
-                    count += print_complete(buf, w, wi,
-                                compl_len - len, compl_char, 256);
-                (*w)(wi, s, len);
-                count += len;
-            }goto print_comp;
-            case 'd':
-                len = itoa(buf, arg_int(mod, ps), decimal_digits, 10);
+        case 's':{
+            const char *s = va_arg(ps, const char*);
+            len = strlen(s);
+            if (compl_char && !align_left)
+                count += print_complete(buf, w, wi,
+                                        compl_len - len, compl_char, 256);
+            (*w)(wi, s, len);
+            count += len;
+        }goto print_comp;
+        case 'd':
+            len = itoa(buf, arg_int(mod, ps), decimal_digits, 10);
             goto print_buf;
-            case 'u':
-                len = utoa(buf, arg_int(mod, ps), decimal_digits, 10);
+        case 'u':
+            len = utoa(buf, arg_int(mod, ps), decimal_digits, 10);
             goto print_buf;
-            case 'x':
-                len = utoa(buf, arg_uint(mod, ps), hex_digits, 16);
+        case 'x':
+            len = utoa(buf, arg_uint(mod, ps), hex_digits, 16);
             goto print_buf;
-            case 'p':
-                len = complete_buf(buf,
-                    utoa(buf, va_arg(ps, uint64_t), hex_digits, 16),
-                    16, '0');
-            print_buf:
-                if (compl_char && !align_left)
-                    count += print_complete(buf, w, wi,
-                                compl_len - len, compl_char,
-                                255 - len);
-                (*w)(wi, buf + 255 - len, len);
-                count += len;
-            print_comp:
-                if (compl_char && align_left)
-                    count += print_complete(buf, w, wi, compl_len - len,
-                                compl_char, 256);
+        case 'p':
+            len = complete_buf(buf,
+                               utoa(buf, va_arg(ps, uint64_t), hex_digits, 16),
+                               16, '0');
+        print_buf:
+            if (compl_char && !align_left)
+                count += print_complete(buf, w, wi,
+                                        compl_len - len, compl_char,
+                                        255 - len);
+            (*w)(wi, buf + 255 - len, len);
+            count += len;
+        print_comp:
+            if (compl_char && align_left)
+                count += print_complete(buf, w, wi, compl_len - len,
+                                        compl_char, 256);
             break;
-            default:
-                return -1;
+        default:
+#ifndef __is_kernel
+            errno = EINVAL;
+#endif
+            return -1;
         }
         ++fmt;
     }
---
+++ tests/run/test_printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <assert.h>
+
+// test bad format
+
+int main() {
+    int rc;
+
+    rc = printf ("%", 1);
+    assert(rc < 0);
+    perror("printf");
+
+    rc = printf("%12", 1);
+    assert(rc < 0);
+    perror("printf");
+}
